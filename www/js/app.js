36f670c0fd6d33f42e516bdffcd44c97539c3d34
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-10-01 14:34:58 -0700

    add entire frontend JS ans HTML temps from old repo

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,105 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('CovalentFitness', ['ionic', 'CovalentFitness.controllers', 'CovalentFitness.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: '../views/menu.html',
+    controller: 'AppCtrl'
+    })
+
+    // Using views because logout is a subview of menu
+    .state('app.logout', {
+      url: '/logout',
+      views: {
+        'menuContent': {
+          templateUrl: '../views/logout.html'
+        }
+      }
+    })
+
+    .state('signupLogin', {
+    url: '/app/signuplogin',
+    abstract: true,
+    templateUrl: '../views/signup.html'
+    })
+
+    .state('signup', {
+    url: '/app/signup',
+    abstract: true,
+    templateUrl: '../views/signup.html',
+    controller: 'SignupCtrl'
+    })
+
+    .state('login', {
+    url: '/app/login',
+    abstract: true,
+    templateUrl: '../views/login.html',
+    controller: 'LoginCtrl'
+    });
+
+//NOTE: none of these are implemented yet!!!!!!!!!!
+
+    // .state('app', {
+    // url: '/app',
+    // abstract: true,
+    // templateUrl: '../views/workouts.html',
+    // controller: 'WorkoutsCtrl'
+    // });
+//   .state('app.search', {
+//     url: '/search',
+//     views: {
+//       'menuContent': {
+//         templateUrl: 'templates/search.html'
+//       }
+//     }
+//   })
+
+//   .state('app.browse', {
+//       url: '/browse',
+//       views: {
+//         'menuContent': {
+//           templateUrl: 'templates/browse.html'
+//         }
+//       }
+//     })
+//     .state('app.playlists', {
+//       url: '/playlists',
+//       views: {
+//         'menuContent': {
+//           templateUrl: 'templates/playlists.html',
+//           controller: 'PlaylistsCtrl'
+//         }
+//       }
+//     })
+
+//   .state('app.single', {
+//     url: '/playlists/:playlistId',
+//     views: {
+//       'menuContent': {
+//         templateUrl: 'templates/playlist.html',
+//         controller: 'PlaylistCtrl'
+//       }
+//     }
+//   });
+//   // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/signuplogin');
+});
