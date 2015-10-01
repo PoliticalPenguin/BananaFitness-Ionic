be7d63377289473cd38cc7792fb2381c8cee19f2
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-10-01 15:45:52 -0700

    clean up code

--- www/js/app.js
+++ www/js/app.js
@@ -24,71 +24,53 @@ angular.module('CovalentFitness', ['ionic', 'CovalentFitness.controllers', 'Cova
     abstract: true,
     templateUrl: '../views/menu.html',
     controller: 'AppCtrl'
-    })
+  })
 
-    // Using views because logout is a subview of menu
-    .state('app.logout', {
-      url: '/logout',
-      views: {
-        'menuContent': {
-          templateUrl: '../views/logout.html'
-        }
+  // Using views because logout is a subview of menu
+  .state('app.logout', {
+    url: '/logout',
+    views: {
+      'menuContent': {
+        templateUrl: '../views/logout.html'
       }
-    })
+    }
+  })
 
-    .state('signupLogin', {
+  .state('signupLogin', {
     url: '/app/signuplogin',
     templateUrl: '../views/signinLogin.html'
-    })
+  })
 
-    .state('signup', {
+  .state('signup', {
     url: '/app/signup',
     templateUrl: '../views/signup.html',
     controller: 'SignupCtrl'
-    })
+  })
 
-    .state('login', {
-    url: '/app/login',
-    templateUrl: '../views/login.html',
-    controller: 'LoginCtrl'
+  .state('login', {
+      url: '/app/login',
+      templateUrl: '../views/login.html',
+      controller: 'LoginCtrl'
     })
     // route to list of workouts
     .state('workouts', {
-    url: '/app/workouts',
-    templateUrl: '../views/workouts.html',
-    controller: 'WorkoutsCtrl'
+      url: '/app/workouts',
+      templateUrl: '../views/workouts.html',
+      controller: 'WorkoutsCtrl'
     })
     // route to individual workout view
     .state('workout', {
-    url: '/app/workout',
-    templateUrl: '../views/workout.html',
-    controller: 'WorkoutCtrl'
+      url: '/app/workout',
+      templateUrl: '../views/workout.html',
+      controller: 'WorkoutCtrl'
     })
     // route to workout editor (for new or edit workout)
     .state('editWorkout', {
-    url: '/app/editWorkout',
-    templateUrl: '../views/addEditWorkout.html',
-    controller: 'WorkoutEditsCtrl'
-  });
-//     .state('app.playlists', {
-//       url: '/playlists',
-//       views: {
-//         'menuContent': {
-//           templateUrl: 'templates/playlists.html',
-//           controller: 'PlaylistsCtrl'
-//         }
-//       }
-//     })
+      url: '/app/editWorkout',
+      templateUrl: '../views/addEditWorkout.html',
+      controller: 'WorkoutEditsCtrl'
+    });
 
-//   .state('app.single', {
-//     url: '/playlists/:playlistId',
-//     views: {
-//       'menuContent': {
-//         templateUrl: 'templates/playlist.html',
-//         controller: 'PlaylistCtrl'
-//       }
-//     }
-//   });
-//   // if none of the above states are matched, use this as the fallback
+  // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/signuplogin');
 });
