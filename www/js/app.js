1d85fc69a5e1aadb8c889f12fed8cbf65afef4e0
Zachary T. Castellanos-Smith <ztsmith25@gmail.com>
2015-10-05 19:49:21 -0700

    final commit

--- www/js/app.js
+++ www/js/app.js
@@ -105,7 +105,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
     controller: 'LogoutCtrl'
   });
 
-  stateProvider.state('workout', {
+  stateProvider.state('/workout', {
     url: '/workout',
     templateUrl: '../views/workouts/workout.html',
     controller: 'WorkoutCtrl'
