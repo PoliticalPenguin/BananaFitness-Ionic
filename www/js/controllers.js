58a0d138a0f51ba8ab9413e07500cacd09dd4341
Nate Willard <nate.a.willard@gmail.com>
2015-10-08 10:12:17 -0700

    Add sample heartrate response

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -90,6 +90,72 @@ contollers.controller('ProfileCtrl', function($scope, $location, $http, Auth) {
 contollers.controller('GraphCtrl', function($scope, $location) {
   $scope.graph = {};
 
+  var response = {
+    "activities-heart": [
+        {
+            "customHeartRateZones": [],
+            "dateTime": "today",
+            "heartRateZones": [
+                {
+                    "caloriesOut": 2.3246,
+                    "max": 94,
+                    "min": 30,
+                    "minutes": 2,
+                    "name": "Out of Range"
+                },
+                {
+                    "caloriesOut": 0,
+                    "max": 132,
+                    "min": 94,
+                    "minutes": 0,
+                    "name": "Fat Burn"
+                },
+                {
+                    "caloriesOut": 0,
+                    "max": 160,
+                    "min": 132,
+                    "minutes": 0,
+                    "name": "Cardio"
+                },
+                {
+                    "caloriesOut": 0,
+                    "max": 220,
+                    "min": 160,
+                    "minutes": 0,
+                    "name": "Peak"
+                }
+            ],
+            "value": "64.2"
+        }
+    ],
+    "activities-heart-intraday": {
+        "dataset": [
+            {
+                "time": "00:00:00",
+                "value": 64
+            },
+            {
+                "time": "00:00:10",
+                "value": 63
+            },
+            {
+                "time": "00:00:20",
+                "value": 64
+            },
+            {
+                "time": "00:00:30",
+                "value": 65
+            },
+            {
+                "time": "00:00:45",
+                "value": 65
+            }
+        ],
+        "datasetInterval": 1,
+        "datasetType": "second"
+    }
+}
+
   $scope.graph.data = [                     // Add bar data, this will set your bars height in the graph
       //Awake
       [16, 15, 20, 12, 16, 12, 8],
